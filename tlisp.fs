eed54d82d3bd825b360b5d2830177809241f3ac1
Steffen Hieber <hieberst@gmx.de>
2000-04-02 11:06:19 +0000

    Wiederaufnahme der Entwicklung

--- tlisp.fs
+++ tlisp.fs
@@ -1,25 +1,25 @@
 \                   TinyLISP (TLISP)
 \
-\              Version 1.0 ref 1997-10-15
+\              Version 1.0 ref 2000-04-02
 \
-\        Written (w) 1993-1997 by Steffen Hieber
+\        Written (w) 1993-2000 by Steffen Hieber
 \
-\        RCS $Id: tlisp.fs,v 1.3 1997-10-16 00:38:29 steffen Exp $
+\        RCS $Id: tlisp.fs,v 1.4 2000-04-02 11:06:19 steffen Exp $
 \
 \
 \ 07.03.1993 TFLOAD aus dem Buch "Forth 83" von Zech uebernommen. Definition
-\            des Vokabulars TLISP. Erstellung der Batch-Datei TLISP.BTM für
+\            des Vokabulars TLISP. Erstellung der Batch-Datei TLISP.BTM fuer
 \            den Start von TLISP unter 4DOS.
 \
-\ 08.03.1993 Das Wort FREE ermittelt die Größe des zur Verfügung stehenden
+\ 08.03.1993 Das Wort FREE ermittelt die Groesse des zur Verfuegung stehenden
 \            freien Speichers.
 \
 \ 09.03.1993 Verbesserung der Batch-Datei TLISP.BTM, die Daten sind jetzt vom
-\            Programm (F83) getrennt. Der Benutzer wird mit HELLO begrüßt.
+\            Programm (F83) getrennt. Der Benutzer wird mit HELLO begruesst.
 \            Die beiden Speicherbereiche LSTMEM und OBJMEM werden angelegt,
-\            ihre Größe ist von den beiden Konstanten MAXLST und MAXOBJ ab-
-\            hängig. Die Variable FREELIST zeigt auf das erste freie Zellen-
-\            paar  im Speicher LSTMEM. Zugriff auf LSTMEM  über die  Worte
+\            ihre Groesse ist von den beiden Konstanten MAXLST und MAXOBJ ab-
+\            haengig. Die Variable FREELIST zeigt auf das erste freie Zellen-
+\            paar  im Speicher LSTMEM. Zugriff auf LSTMEM  ueber die  Worte
 \            (CAR), (CDR), (RPLACA) und (RPLACD). Der Listen-Speicher wird
 \            mittels INIT initialisiert. NIL.
 \
@@ -28,32 +28,36 @@
 \
 \ ===========================================================================
 \
-\ 15.10.1997 Umstellung des bisherigen Codes auf GNU Forth Version 0.3.0.
+\ 15.10.1997 Umstellung des bisherigen Codes auf GNU Forth.
+\
 \            Der Eaker-Case ist bereits in GForth implementiert, auch TFLOAD
-\            ist nicht mehr nötig. Den F83-eigenen Alias (S habe ich durch
+\            ist nicht mehr noetig. Den F83-eigenen Alias (S habe ich durch
 \            den Standard ( ersetzt. Das Wort FREE war falsch und zudem noch
 \            redundant, da mit UNUSED der freie Speicher ermittelt werden
-\            kann - entfernt. Umstellung der Adreßarithmetik auf die Worte
+\            kann - entfernt. Umstellung der Adressarithmetik auf die Worte
 \            CELL, CELLS und CHARS. Erzeugung des Shell-Skripts TLISP zum
 \            Start von GForth und TLISP.
 \
 \ 16.10.1997 Kontrolle der Programmentwicklung durch das RCS gestartet. 
+\
+\ 02.04.2000 Wiederaufnahme der Entwicklung.
+\
 
 
 FORTH DEFINITIONS       \ Grund-Vokabular erweitern ...
 
 VOCABULARY tlisp        \ Vokabular TLISP anlegen
-tlisp DEFINITIONS       \ alle neuen Wörter ab jetzt in das Vokabular TLISP
+tlisp DEFINITIONS       \ alle neuen Woerter ab jetzt in das Vokabular TLISP
 
 
-: hello ( -- )          \ Begrüßung des Anwenders
+: hello ( -- )          \ Begruessung des Anwenders
 \ =====
-    CR ." TLISP Version 1.0 ref 1997-10-15"
-    CR ." Copyright (C) 1993-1997 Steffen Hieber"
+    CR ." TLISP Version 1.0 ref 2000-04-02"
+    CR ." Copyright (C) 1993-2000 Steffen Hieber"
 ;
 
 
-2048 CONSTANT maxobj    \ Größe des Objekt-Speichers OBJMEM in Zeichen
+2048 CONSTANT maxobj    \ Groesse des Objekt-Speichers OBJMEM in Zeichen
 4096 CONSTANT maxlst    \ Anzahl der vorhandenen Zellenpaare in LSTMEM
 
 
