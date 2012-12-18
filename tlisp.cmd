497669de2c7c87d50f301da2b10593fed8680769
Steffen Hieber <hieberst@gmx.de>
2012-12-18 21:02:09 +0000

    minor update

--- tlisp.cmd
+++ tlisp.cmd
@@ -1,10 +1,11 @@
 @echo off
 
-rem $Id: tlisp.cmd,v 1.4 2010-11-19 08:37:01 steffen Exp $
+rem $Id: tlisp.cmd,v 1.5 2012-12-18 21:02:09 steffen Exp $
 
-if not defined GFORTHPATH set GFORTHPATH=%ProgramFiles%\gforth
+if not defined GFORTH set GFORTH=%ProgramFiles%\gforth\gforth.exe
 
-rem "%GFORTHPATH%\gforth.exe" -p .
-"%GFORTHPATH%\gforth.exe" -p . tlisp.fs
-pause
-
+cd /d %~dp0
+rem "%GFORTH%" "%ProgramFiles%\gforth\ans-report.fs" tlisp.fs -e "print-ans-report bye"
+"%GFORTH%" tlisp.fs -e "driver bye"
+rem "%GFORTH%" tlisp.fs tests.fs
+rem pause
