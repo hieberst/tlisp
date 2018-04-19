9e02da24cb503d1e43841181bc519c846faba6c2
Steffen Hieber <hieberst@gmx.de>
2018-04-19 06:51:02 +0000

    disable warnings in Gforth 0.7.9_20171101

--- tlisp.cmd
+++ tlisp.cmd
@@ -5,7 +5,7 @@ rem $Id$
 if not defined GFORTH set GFORTH=%ProgramFiles%\gforth\gforth.exe
 
 cd /d %~dp0
-"%GFORTH%" tlisp.fs -e "driver"
+"%GFORTH%" -e "warnings off" tlisp.fs -e "driver"
 
 rem "%GFORTH%" "%ProgramFiles%\gforth\ans-report.fs" tlisp.fs -e "print-ans-report bye"
 rem "%GFORTH%" tlisp.fs tests.fs
