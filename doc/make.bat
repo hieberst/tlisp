ac63789ff484305ffb8e3e4964408249b78f419d
Steffen Hieber <hieberst@gmx.de>
2023-03-20 19:38:22 +0100

    Fixed funny typo

--- doc/make.bat
+++ doc/make.bat
@@ -1,6 +1,6 @@
 @echo off
 if not defined VCINSTALLDIR set VCINSTALLDIR=%ProgramFiles%\Microsoft Visual Studio 9.0\VC
-if exist call "%VCINSTALLDIR%\bin\vcvars32.bat" "%VCINSTALLDIR%\bin\vcvars32.bat"
+if exist "%VCINSTALLDIR%\bin\vcvars32.bat" call "%VCINSTALLDIR%\bin\vcvars32.bat"
 nmake clean RM=del
 nmake
 pause
